26d63b06dff4f58516f29d336c016d8b88fa70c4
ilkatkov <ilkatkov@yandex.ru>
2021-09-09 20:26:35 +0300

    remove modules, fix tests

--- marusia/index.php
+++ marusia/index.php
@@ -10,8 +10,8 @@ $version = $data['version'];
 
 // кнопки
 
-$lessons_buttons = array(array('title' => "Вывод данных"), array('title' => 'Ввод данных'), array('title' => 'Переменные'), array('title' => 'Типы данных'), array('title' => 'Условный оператор'), array('title' => 'Циклы'), array('title' => 'Функции'), array('title' => 'Модули'), array('title' => 'Тестирование'));
-$test_buttons = array(array('title' => "Вывод данных"), array('title' => 'Ввод данных'), array('title' => 'Переменные'), array('title' => 'Типы данных'), array('title' => 'Условный оператор'), array('title' => 'Циклы'), array('title' => 'Функции'), array('title' => 'Модули'), array('title' => 'Лекции'));
+$lessons_buttons = array(array('title' => "Вывод данных"), array('title' => 'Ввод данных'), array('title' => 'Переменные'), array('title' => 'Типы данных'), array('title' => 'Условный оператор'), array('title' => 'Циклы'), array('title' => 'Функции'),  array('title' => 'Тестирование'));
+$test_buttons = array(array('title' => "Вывод данных"), array('title' => 'Ввод данных'), array('title' => 'Переменные'), array('title' => 'Типы данных'), array('title' => 'Условный оператор'), array('title' => 'Циклы'), array('title' => 'Функции'),  array('title' => 'Лекции'));
 $start_buttons = array(array('title' => "Лекции"), array('title' => 'Тестирование'));
 
 
@@ -23,7 +23,6 @@ $types = 'типы';
 $operator = 'условный';
 $cicles = 'циклы';
 $functions = 'функции';
-$modules = 'модули';
 $lessons = 'лекции';
 $testing = 'тестирование';
 
@@ -36,13 +35,11 @@ if (is_null($data['state']['session']['active'])) {
         $teach = array('учить', 'учиться', 'основы', 'узнать', 'обучить', 'обучение', 'лекция', 'лекции');
         $test = array('тест', 'тестирование', 'знания', 'проверка', 'тестировать', 'проверить');
         if (in_array($choice, $teach)) {
-            $response = array("session" => $session, "version" => $version, "response" => array("text" => "Выбери тему урока. \n{1.}{} Вывод данных\n{2.}{} Ввод данных\n{3.}{} Переменные\n{4.}{} Типы данных\n{5.}{} Условный оператор\n{6.}{} Циклы\n{7.}{} Функции\n{8.}{} Модули", 'buttons' => $lessons_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "lessons"));
+            $response = array("session" => $session, "version" => $version, "response" => array("text" => "Выбери тему урока. \n{1.}{} Вывод данных\n{2.}{} Ввод данных\n{3.}{} Переменные\n{4.}{} Типы данных\n{5.}{} Условный оператор\n{6.}{} Циклы\n{7.}{} Функции", 'buttons' => $lessons_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "lessons"));
         } else if (in_array($choice, $test)) {
-            $response = array("session" => $session, "version" => $version, "response" => array("text" => "Выбери тему тестирования. \n{1.}{} Вывод данных\n{2.}{} Ввод данных\n{3.}{} Переменные\n{4.}{} Типы данных\n{5.}{} Условный оператор\n{6.}{} Циклы\n{7.}{} Функции\n{8.}{} Модули", 'buttons' => $test_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test"));
-        }
-        else {
+            $response = array("session" => $session, "version" => $version, "response" => array("text" => "Выбери тему тестирования. \n{1.}{} Вывод данных\n{2.}{} Ввод данных\n{3.}{} Переменные\n{4.}{} Типы данных\n{5.}{} Условный оператор\n{6.}{} Циклы\n{7.}{} Функции", 'buttons' => $test_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test"));
+        } else {
             $response = array("session" => $session, "version" => $version, "response" => array("text" => "Выбирай{:}{\n} лекции или тестирование!", 'buttons' => $start_buttons, "end_session" => false), 'session_state' => array("active" => true));
-      
         }
     } else if ($data['state']['session']['choice'] == "lessons") {
         $lesson_theme = $data['request']['nlu']['tokens'];
@@ -68,13 +65,9 @@ if (is_null($data['state']['session']['active'])) {
         } else if (in_array($functions, $lesson_theme)) {
             $text = getArticle("Функции");
             $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, 'buttons' => $lessons_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "lessons"));
-        } else if (in_array($modules, $lesson_theme)) {
-            $text = getArticle("Модули");
-            $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, 'buttons' => $lessons_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "lessons"));
         } else if (in_array($testing, $lesson_theme)) {
             $response = array("session" => $session, "version" => $version, "response" => array("text" => "Выбери тему тестирования. \n{1.}{} Вывод данных\n{2.}{} Ввод данных\n{3.}{} Переменные\n{4.}{} Типы данных\n{5.}{} Условный оператор\n{6.}{} Циклы\n{7.}{} Функции\n{8.}{} Модули", 'buttons' => $test_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test"));
-        }
-        else {
+        } else {
             $response = array("session" => $session, "version" => $version, "response" => array("text" => "Выбери тему урока. \n{1.}{} Вывод данных\n{2.}{} Ввод данных\n{3.}{} Переменные\n{4.}{} Типы данных\n{5.}{} Условный оператор\n{6.}{} Циклы\n{7.}{} Функции\n{8.}{} Модули", 'buttons' => $lessons_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "lessons"));
         }
     } else if ($data['state']['session']['choice'] == "test" && is_null($data['state']['session']['test'])) {
@@ -95,36 +88,31 @@ if (is_null($data['state']['session']['active'])) {
             $questions = getQuestions("Переменные");
             $question_buttons = array(array('title' => $questions[$question]['option1']), array('title' => $questions[$question]['option2']), array('title' => $questions[$question]['option3']), array('title' => $questions[$question]['option4']));
             $text = (string)($question + 1) . "й вопрос: " . $questions[$question]['question'] . "\nВарианты ответа: \n{1. }{}" . $questions[$question]['option1'] . "\n{2. }{}" . $questions[$question]['option2'] . "\n{3. }{}" . $questions[$question]['option3'] . "\n{4. }{}" . $questions[$question]['option4'];
-            $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, "buttons" => $question_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test", "test" => "Переменные", "points" => 0, "question" => 0));
+            $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, "buttons" => $question_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test", "test" => "Переменные", "points" => 0, "question" => 0, "ready_for_answer" => true));
         } else if (in_array($types, $test_theme)) {
             $questions = getQuestions("Типы данных");
             $question_buttons = array(array('title' => $questions[$question]['option1']), array('title' => $questions[$question]['option2']), array('title' => $questions[$question]['option3']), array('title' => $questions[$question]['option4']));
             $text = (string)($question + 1) . "й вопрос: " . $questions[$question]['question'] . "\nВарианты ответа: \n{1. }{}" . $questions[$question]['option1'] . "\n{2. }{}" . $questions[$question]['option2'] . "\n{3. }{}" . $questions[$question]['option3'] . "\n{4. }{}" . $questions[$question]['option4'];
-            $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, "buttons" => $question_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test", "test" => "Типы данных", "points" => 0, "question" => 0));
+            $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, "buttons" => $question_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test", "test" => "Типы данных", "points" => 0, "question" => 0, "ready_for_answer" => true));
         } else if (in_array($operator, $test_theme)) {
             $questions = getQuestions("Условный оператор");
             $question_buttons = array(array('title' => $questions[$question]['option1']), array('title' => $questions[$question]['option2']), array('title' => $questions[$question]['option3']), array('title' => $questions[$question]['option4']));
             $text = (string)($question + 1) . "й вопрос: " . $questions[$question]['question'] . "\nВарианты ответа: \n{1. }{}" . $questions[$question]['option1'] . "\n{2. }{}" . $questions[$question]['option2'] . "\n{3. }{}" . $questions[$question]['option3'] . "\n{4. }{}" . $questions[$question]['option4'];
-            $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, "buttons" => $question_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test", "test" => "Условный оператор", "points" => 0, "question" => 0));
+            $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, "buttons" => $question_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test", "test" => "Условный оператор", "points" => 0, "question" => 0, "ready_for_answer" => true));
         } else if (in_array($cicles, $test_theme)) {
             $questions = getQuestions("Циклы");
             $question_buttons = array(array('title' => $questions[$question]['option1']), array('title' => $questions[$question]['option2']), array('title' => $questions[$question]['option3']), array('title' => $questions[$question]['option4']));
             $text = (string)($question + 1) . "й вопрос: " . $questions[$question]['question'] . "\nВарианты ответа: \n{1. }{}" . $questions[$question]['option1'] . "\n{2. }{}" . $questions[$question]['option2'] . "\n{3. }{}" . $questions[$question]['option3'] . "\n{4. }{}" . $questions[$question]['option4'];
-            $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, "buttons" => $question_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test", "test" => "Циклы", "points" => 0, "question" => 0));
+            $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, "buttons" => $question_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test", "test" => "Циклы", "points" => 0, "question" => 0, "ready_for_answer" => true));
         } else if (in_array($functions, $test_theme)) {
             $questions = getQuestions("Функции");
             $question_buttons = array(array('title' => $questions[$question]['option1']), array('title' => $questions[$question]['option2']), array('title' => $questions[$question]['option3']), array('title' => $questions[$question]['option4']));
             $text = (string)($question + 1) . "й вопрос: " . $questions[$question]['question'] . "\nВарианты ответа: \n{1. }{}" . $questions[$question]['option1'] . "\n{2. }{}" . $questions[$question]['option2'] . "\n{3. }{}" . $questions[$question]['option3'] . "\n{4. }{}" . $questions[$question]['option4'];
-            $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, "buttons" => $question_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test", "test" => "Функции", "points" => 0, "question" => 0));
-        } else if (in_array($modules, $test_theme)) {
-            $questions = getQuestions("Модули");
-            $question_buttons = array(array('title' => $questions[$question]['option1']), array('title' => $questions[$question]['option2']), array('title' => $questions[$question]['option3']), array('title' => $questions[$question]['option4']));
-            $text = (string)($question + 1) . "й вопрос: " . $questions[$question]['question'] . "\nВарианты ответа: \n{1. }{}" . $questions[$question]['option1'] . "\n{2. }{}" . $questions[$question]['option2'] . "\n{3. }{}" . $questions[$question]['option3'] . "\n{4. }{}" . $questions[$question]['option4'];
-            $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, "buttons" => $question_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test", "test" => "Модули", "points" => 0, "question" => 0));
+            $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, "buttons" => $question_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test", "test" => "Функции", "points" => 0, "question" => 0, "ready_for_answer" => true));
         } else if (in_array($lessons, $test_theme)) {
-            $response = array("session" => $session, "version" => $version, "response" => array("text" => "Выбери тему урока. \n{1.}{} Вывод данных\n{2.}{} Ввод данных\n{3.}{} Переменные\n{4.}{} Типы данных\n{5.}{} Условный оператор\n{6.}{} Циклы\n{7.}{} Функции\n{8.}{} Модули", 'buttons' => $lessons_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "lessons"));
+            $response = array("session" => $session, "version" => $version, "response" => array("text" => "Выбери тему урока. \n{1.}{} Вывод данных\n{2.}{} Ввод данных\n{3.}{} Переменные\n{4.}{} Типы данных\n{5.}{} Условный оператор\n{6.}{} Циклы\n{7.}{} Функции", 'buttons' => $lessons_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "lessons"));
         } else {
-            $text = "Выбери тему тестирования. \n{1.}{} Вывод данных\n{2.}{} Ввод данных\n{3.}{} Переменные\n{4.}{} Типы данных\n{5.}{} Условный оператор\n{6.}{} Циклы\n{7.}{} Функции\n{8.}{} Модули";
+            $text = "Выбери тему тестирования. \n{1.}{} Вывод данных\n{2.}{} Ввод данных\n{3.}{} Переменные\n{4.}{} Типы данных\n{5.}{} Условный оператор\n{6.}{} Циклы\n{7.}{} Функции";
             $response = array("session" => $session, "version" => $version, "response" => array("text" => $text, 'buttons' => $test_buttons, "end_session" => false), 'session_state' => array("active" => true, "choice" => "test"));
         }
     } else if (!is_null($data['state']['session']['test'])) {
